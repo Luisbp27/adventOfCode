d7963ccf390450f68d42d20db4a74c98a00f73c3
Luis Barca <luisbarcapons@gmail.com>
2022-05-03 19:59:45 +0200

    Update day1

--- day1/day1.py
+++ day1/day1.py
@@ -1,16 +1,27 @@
 import csv
+import numpy as np
+
 
 def __main__():
     with open("data.txt", "r", encoding="utf-8") as f:
         data = list(csv.reader(f, delimiter='\t'))
-    
+
+    data = np.array(data).astype(int)
     counter = 0
-    i = 1
-    while i < len(data):
+
+    print(part1(data, counter))
+    print(part2(data, counter))
+
+
+def part1(data, counter):
+    for i in range(len(data)):
         if data[i] > data[i-1]:
             counter += 1
-        i += 1
-    print(counter)
+    return counter
+
+
+def part2(data, counter):
+    pass
 
 if __name__ == '__main__':
     __main__()
