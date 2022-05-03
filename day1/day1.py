0933b6814ce531074f53eabb993a02384fdfee15
Luis Barca <luisbarcapons@gmail.com>
2022-05-03 20:56:20 +0200

    day 1 part 2

--- day1/day1.py
+++ day1/day1.py
@@ -3,10 +3,12 @@ import numpy as np
 
 
 def __main__():
+    """" Main function """
+
     with open("data.txt", "r", encoding="utf-8") as f:
         data = list(csv.reader(f, delimiter='\t'))
 
-    data = np.array(data).astype(int)
+    data = [int(data[i][0]) for i in range(len(data))]
     counter = 0
 
     print(part1(data, counter))
@@ -17,11 +19,21 @@ def part1(data, counter):
     for i in range(len(data)):
         if data[i] > data[i-1]:
             counter += 1
+
     return counter
 
 
 def part2(data, counter):
-    pass
+    j = 0
+    i = 0
+    sum_group = []
+
+    while i < (len(data) - 3):
+        sum_group.append(data[i] + data[i+1] + data[i+2])
+        i += 3
+        j += 1
+    return part1(sum_group, counter)
+
 
 if __name__ == '__main__':
     __main__()
