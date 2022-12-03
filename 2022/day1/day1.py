f37de1b37a0a1444c19563333a055e3d0d8239d2
Luis Barca <luisbarcapons@gmail.com>
2022-12-03 23:15:58 +0100

    Update 03/12 Day 1

---
+++ 2022/day1/day1.py
@@ -0,0 +1,18 @@
+
+with open("data.txt") as f:
+    data = f.read().split("\n")
+    calories = list()
+    sum_calories = 0
+    
+    for i in data:
+        if i == "":
+            calories.append(sum_calories)
+            sum_calories = 0
+
+        else:
+            sum_calories += int(i)
+
+    calories.sort()
+    
+print(calories[-1]) # Problem 1
+print(sum(calories[-3:])) # Problem 2
